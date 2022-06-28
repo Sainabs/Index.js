22b619d6a2963e44a41552adc4006d9d914d51c0
Sainabs <Sainab.sahal1@gmail.com>
2022-06-28 01:39:32 -0500

    index-lab.js

---
+++ firstday challenge1.1.js
@@ -0,0 +1,23 @@
+console.log("helloworld")
+//coding challenges
+const x = 5
+const y = 5
+
+if (x < y){
+    console.log(y)
+} else{
+        console.log (x) 
+    }
+//advanced coding challenges
+if (x < y){
+    console.log(y)
+    }else if (x === y){
+        console.log("The values are identical") 
+    } else{
+        console.log (x) 
+    }
+
+
+//for (let x=1; y=2; i++){
+  ////  console.log (=2)
+//}
